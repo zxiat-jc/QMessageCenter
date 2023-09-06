489cde355cc329e56de4410d793ce960c33fa3d7
WindSnowLi <wzmmdsnn@163.com>
2023-09-06 17:51:44 +0800

    消息包装器

---
+++ QMessageCenter/PackageMessage.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include <QObject>
+
+template <typename T>
+class Package : public QObject {
+
+private:
+    T _data;
+
+public:
+    Package(T data)
+        : QObject(nullptr)
+        , _data(data)
+    {
+    }
+
+    inline T data() const
+    {
+        return _data;
+    }
+};
